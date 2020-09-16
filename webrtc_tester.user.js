4758cb51c030d9c70789edcd3c2edbe589dcb9a1
Maxim Bezrukavnikov <60591825+m-bezrukavnikov-hotlead-io@users.noreply.github.com>
2020-09-16 13:13:40 +1000

    Create webrtc_tester.user.js

---
+++ webrtc_tester.user.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         WebRTC Debuger
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://crm.hotlead.io/telephony/web-rtc/sip-accounts
+// @match        http://localhost:8080/telephony/web-rtc/sip-accounts
+// @downloadURL  https://github.com/DEMENT0R/GoD_helper/raw/master/god_helper.user.js
+// @updateURL    https://github.com/DEMENT0R/GoD_helper/raw/master/god_helper.user.js
+// @run-at       document-start
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var unsafeWindow = window.wrappedJSObject;
+
+    document.getElementById("webrtc-wrapper").innerHTML += "<div id = 'webrtc-debugger-output' style='max-height: 200px; overflow: auto;'></div>";
+
+    function loop(){
+        if (unsafeWindow.webrtcWidget.webrtcBox.phoneEngine.session){
+            var from = unsafeWindow.webrtcWidget.webrtcBox.phoneEngine.session._request.from;
+            output("_display_name: " + from._display_name + ", _user: " + from._uri._user);
+        }
+    }
+
+    function output(message){
+        document.getElementById("webrtc-debugger-output").innerHTML = message + "<br>" + document.getElementById("webrtc-debugger-output").innerHTML;
+    }
+
+    // даём время на установку сессии
+    setTimeout(
+        setInterval(
+            loop, 1000
+        ), 3000
+    );
+})();
