7a5edc63972c0d72484b7689cd1210bcb7408abd
Maxim Bezrukavnikov <60591825+m-bezrukavnikov-hotlead-io@users.noreply.github.com>
2020-09-16 16:48:25 +1000

    Update webrtc_tester.user.js

--- webrtc_tester.user.js
+++ webrtc_tester.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WebRTC Debuger
 // @namespace    http://tampermonkey.net/
-// @version      0.1.5
+// @version      0.1.6
 // @description  try to take over the world!
 // @author       You
 // @match        https://crm.hotlead.io/telephony/web-rtc/sip-accounts
@@ -22,23 +22,17 @@
     // и скрываем хинт
     document.getElementById("dialer-input-hint").style.display = "none";
 
-    // имя и номер контакта
-    var contactName = document.getElementById("dialer-call-contact");
-    var contactNumber = document.getElementById("dialer-call-number");
-    contactName.innerHTML = "contactName";
-    contactNumber.innerHTML = "contactNumber";
-
     document.getElementById("webrtc-wrapper").innerHTML += "<div id = 'webrtc-debugger-output' style='max-height: 200px; max-width: 400px; overflow: auto;'></div>";
 
     function loop(){
         if (unsafeWindow.webrtcWidget.webrtcBox.phoneEngine.session){
             var from = unsafeWindow.webrtcWidget.webrtcBox.phoneEngine.session._request.from;
             output("_display_name: " + from._display_name + ", _uri._user: " + from._uri._user);
-            contactName.innerHTML = from._display_name;
-            contactNumber.innerHTML = from._uri._user;
+            document.getElementById("dialer-call-contact").innerHTML = from._display_name;
+            document.getElementById("dialer-call-number").innerHTML = from._uri._user;
         } else {
-            contactName.innerHTML = null;
-            contactNumber.innerHTML = null;
+            document.getElementById("dialer-call-contact").innerHTML = null;
+            document.getElementById("dialer-call-number").innerHTML = null;
         }
     }
 
